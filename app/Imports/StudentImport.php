537df3f07972e7da382cc3256368f5daecd23846
divyeshz <divyeshm@zignuts.com>
2023-12-14 13:59:06 +0530

    Refactor StudentImport to Calculate and Store Result Data

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\Result;
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -13,7 +14,8 @@ class StudentImport implements ToModel, WithHeadingRow
      */
     public function model(array $rows)
     {
-        return new Student([
+        // Extract data for the Student table
+        $studentData = [
             "roll_no" => $rows["roll_no"],
             "name" => $rows["name"],
             "class" => $rows["class"],
@@ -24,6 +26,59 @@ class StudentImport implements ToModel, WithHeadingRow
             "city" => $rows["city"],
             "state" => $rows["state"],
             "pincode" => $rows["pincode"],
-        ]);
+        ];
+
+        // Insert data into the Student table
+        $student = Student::create($studentData);
+
+        $total = $this->totalMarks($rows);
+
+        $percentage = $this->calculatePercentage($rows);
+
+        // Extract data for the Result table
+        $resultData = [
+            "std_id" => $student->id,
+            "maths" => $rows["maths"],
+            "science" => $rows["science"],
+            "hindi" => $rows["hindi"],
+            "english" => $rows["english"],
+            "social_science" => $rows["social_science"],
+            "computer" => $rows["computer"],
+            "arts" => $rows["arts"],
+            "total" => $total,
+            "percentage" => $percentage,
+        ];
+
+        // Insert data into the Result table
+        Result::create($resultData);
+    }
+
+    private function totalMarks(array $rows)
+    {
+        $subjects = ['maths', 'science', 'hindi', 'english', 'social_science', 'computer', 'arts'];
+
+        $obtainedMarks = 0;
+        foreach ($subjects as $subject) {
+            $obtainedMarks += isset($rows[$subject]) ? $rows[$subject] : 0;
+        }
+        return intval($obtainedMarks);
+    }
+
+    private function calculatePercentage(array $rows)
+    {
+        $subjects = ['maths', 'science', 'hindi', 'english', 'social_science', 'computer', 'arts'];
+        $totalMarks = 0;
+        $obtainedMarks = 0;
+
+        foreach ($subjects as $subject) {
+            $totalMarks += 100;
+            $obtainedMarks += isset($rows[$subject]) ? $rows[$subject] : 0;
+        }
+
+        if ($totalMarks === 0) {
+            return 0; // To avoid division by zero
+        }
+
+        return ($obtainedMarks / $totalMarks) * 100;
     }
 }
