3a155bb2468f2922cfd7d51ce4c6e5168875669d
divyeshz <divyeshm@zignuts.com>
2023-12-18 12:54:33 +0530

    Refactor schedule value validation logic

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -17,7 +17,22 @@ class ScheduleController extends Controller
         // validation
         $request->validate([
             'schedule_type'     => 'required|string|in:student,class',
-            'schedule_value'    => 'required|string',
+            'schedule_value' => [
+                'required',
+                function ($attribute, $value, $fail) use ($request) {
+                    if ($request->schedule_type === 'class') {
+                        // Validate numeric value between 1 and 12
+                        if (!is_numeric($value) || $value < 1 || $value > 12) {
+                            $fail('The ' . $attribute . ' must be a numeric value between 1 and 12.');
+                        }
+                    } elseif ($request->schedule_type === 'student') {
+                        // Validate using regex pattern for student type
+                        if (!preg_match('/^STD_\d{5}$/', $value)) {
+                            $fail('The ' . $attribute . ' must match the pattern STD_XXXXX');
+                        }
+                    }
+                }
+            ],
             'datetime'          => 'required|date_format:Y-m-d H:i:s',
         ]);
 
